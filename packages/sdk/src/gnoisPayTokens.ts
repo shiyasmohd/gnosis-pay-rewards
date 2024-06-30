a135ade1da74a4415c3bca06d9e4b0ef83662553
Adam Azad <adam@adamazad.com>
2024-06-30 18:32:42 +0100

    feat(packages/sdk): add gnosisPayTokens

--- packages/sdk/src/gnoisPayTokens.ts
+++ packages/sdk/src/gnoisPayTokens.ts
@@ -1,27 +1,30 @@
 import { Address, isAddressEqual } from 'viem';
 
-type SimpleErc20TokenType = {
-  symbol: string;
+export type SerializableErc20TokenType = {
   address: Address;
+  symbol: string;
   decimals: number;
   name: string;
+  chainId: number;
 };
 
-const tokens: SimpleErc20TokenType[] = [
+export const gnosisPayTokens: SerializableErc20TokenType[] = [
   {
     symbol: 'EURe',
     address: '0xcB444e90D8198415266c6a2724b7900fb12FC56E',
     decimals: 18,
     name: 'Monerium EUR emoney (EURe)',
+    chainId: 100,
   },
   {
     symbol: 'GBPe',
     address: '0x5cb9073902f2035222b9749f8fb0c9bfe5527108',
     decimals: 18,
     name: 'Monerium GBP emoney (GBPe)',
+    chainId: 100,
   },
 ];
 
-export function getGnosisPayToken(address: Address) {
-  return tokens.find((token) => isAddressEqual(token.address, address));
+export function getGnosisPayTokenByAddress(tokenAddress: Address) {
+  return gnosisPayTokens.find((token) => isAddressEqual(token.address, tokenAddress));
 }
--- packages/sdk/src/index.ts
+++ packages/sdk/src/index.ts
@@ -2,3 +2,4 @@ export * from './utils';
 export * from './core';
 export * from './contracts';
 export * from './calcRewardAmount';
+export * from './gnoisPayTokens';
