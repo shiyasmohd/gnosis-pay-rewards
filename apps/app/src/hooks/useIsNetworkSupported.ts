45a3afd69b7d4b67bd2be449a5f1e9f99c9db605
Adam Azad <adam@adamazad.com>
2024-07-03 00:11:52 +0100

    fix(app/app): `useChainId`

--- apps/app/src/hooks/useIsNetworkSupported.ts
+++ apps/app/src/hooks/useIsNetworkSupported.ts
@@ -1,8 +1,9 @@
-import { useNetwork } from 'wagmi';
+import { gnosis } from 'viem/chains';
+import { useChainId } from 'wagmi';
 
 export function useIsNetworkSupported() {
-  const { chain } = useNetwork();
-  const isNetworkSupported = chain && chain.unsupported === false ? true : false;
+  const chainId = useChainId();
+  const isNetworkSupported = chainId && chainId === gnosis.id ? true : false;
   return {
     isNetworkSupported,
   };
