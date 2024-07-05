3c4523aa86b5afaf184757f5167ac7c33b8161c3
Adam Azad <adam@adamazad.com>
2024-07-05 09:49:39 +0100

    chore(pacakes/sdk): more events

--- packages/sdk/src/socket.ts
+++ packages/sdk/src/socket.ts
@@ -1,16 +1,23 @@
 import type { Server as SocketIoServer } from 'socket.io';
 
 import { PendingRewardFieldsTypePopulated } from './database/pendingReward';
+import { WeekSnapshotDocumentFieldsType } from './database/weekData';
 
 export interface GnosisPayRewardsServerToClientEventsType {
   newPendingReward: (data: PendingRewardFieldsTypePopulated) => void;
   recentPendingRewards: (data: PendingRewardFieldsTypePopulated[]) => void;
-  pong: () => void;
+  currentWeekData: (data: WeekSnapshotDocumentFieldsType) => void;
+  currentWeekDataUpdated: (data: WeekSnapshotDocumentFieldsType) => void;
+  weekDataByTimestamp: (data: WeekSnapshotDocumentFieldsType | null) => void;
+  allWeekData: (data: WeekSnapshotDocumentFieldsType[]) => void;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-interface
 export type GnosisPayRewardsClientToServerEventsType = {
   getRecentPendingRewards: (limit: number) => void;
+  getCurrentWeekData: () => void;
+  getWeekDataByTimestamp: (weekTimestamp: number) => void;
+  getAllWeekData: () => void;
 };
 
 export interface GnosisPayRewardsInterServerEventsType {
