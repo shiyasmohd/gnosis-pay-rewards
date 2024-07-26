237832f19de34138e77ae82587f7b497d983b52e
Adam Azad <adam@adamazad.com>
2024-07-26 14:25:14 +0100

    chore(packages/sdk): add publish.sh

---
+++ packages/sdk/publish.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+if [ -z "$NPM_TOKEN" ]; then
+  echo "Error: NPM_TOKEN environment variable is not set."
+  exit 1
+fi
+
+function revert_changes() {
+  echo "${original_npmrc_content}" > .npmrc
+  echo "${original_package_json_content}" > package.json
+}
+
+# Get the original name from package.json
+original_npmrc_content=$(cat .npmrc)
+original_package_json_content=$(cat package.json)
+# Update the name in package.json to match the GitHub package name
+target_name="@kpkpkg\/gnosis-pay-rewards-sdk" # @scope must match the repo owner on GitHub
+
+(
+  # Update the name in package.json to match the GitHub package name
+  sed -i '' 's/"name":.*/"name": "'$target_name'",/' package.json
+
+  # configure token
+  echo "//registry.npmjs.org/:_authToken=${NPM_TOKEN}" >> .npmrc
+
+  # Publish the package
+  npm publish --access=public --no-workspaces
+
+  revert_changes;
+) || (
+  echo "Error publishing package"
+  revert_changes;
+)
