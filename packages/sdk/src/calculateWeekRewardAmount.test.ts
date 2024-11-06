bb43f067929cc5ccadcc2dd391e8edbf69343cae
Adam Azad <adam@adamazad.com>
2024-11-06 09:31:14 +0000

    chore(sdk): more test cases

--- packages/sdk/src/calculateWeekRewardAmount.test.ts
+++ packages/sdk/src/calculateWeekRewardAmount.test.ts
@@ -3,8 +3,8 @@ import { describe, expect, test } from '@jest/globals';
 import { calculateWeekRewardAmount } from './calculateWeekRewardAmount';
 
 describe('calculateWeekRewardAmount', () => {
-  const fourWeeksUsdVolumeThreshold = 22_000; // using the USDC threshold for tests
-  const gnoUsdPrice = 200;
+  const fourWeeksUsdVolumeThreshold = 22_000 as const; // using the USDC threshold for tests
+  const gnoUsdPrice = 200 as const;
 
   const cases = [
     { gnoBalance: 100, expectedRewardPercentage: 4 },
@@ -17,7 +17,7 @@ describe('calculateWeekRewardAmount', () => {
     const weekUsdVolume = 7_000;
     const fourWeeksUsdVolume = 23_000;
 
-    test(`returns ${expectedRewardPercentage}% for ${gnoBalance} GNO balance`, () => {
+    test(`${gnoBalance} GNO balance: ${expectedRewardPercentage}% for non-OG NFT holders`, () => {
       const rewardAmount = calculateWeekRewardAmount({
         gnoUsdPrice,
         fourWeeksUsdVolumeThreshold,
@@ -30,7 +30,7 @@ describe('calculateWeekRewardAmount', () => {
       expect(actualRewardPercentage).toBe(expectedRewardPercentage);
     });
 
-    test(`returns ${expectedRewardPercentage}+1% when ${gnoBalance} GNO balance for OG NFT holders `, () => {
+    test(`${gnoBalance} GNO balance: ${expectedRewardPercentage}+1% for OG NFT holders `, () => {
       const rewardAmount = calculateWeekRewardAmount({
         gnoUsdPrice,
         fourWeeksUsdVolumeThreshold,
@@ -45,31 +45,85 @@ describe('calculateWeekRewardAmount', () => {
     });
   }
 
-  test('returns 0 if the week volume is above the threshold', () => {
+  test('1-week volume above 4-weeks threshold ($22,000): 0%', () => {
     const rewardAmount = calculateWeekRewardAmount({
       gnoUsdPrice,
       fourWeeksUsdVolumeThreshold,
       gnoBalance: 100,
       isOgNftHolder: false,
-      weekUsdVolume: 22_001,
-      fourWeeksUsdVolume: 23_000,
+      weekUsdVolume: 22_000.1,
+      fourWeeksUsdVolume: 22_000,
     });
     expect(rewardAmount).toBe(0);
   });
 
-  test('returns 0 if the past 4 weeks volume is above the threshold and the week volume is greater than the threshold', () => {
+  test('same 1-week and 4-weeks volumes ($22,000.1) above threshold ($22,000), 100 GNO balance: 0%', () => {
     const rewardAmount = calculateWeekRewardAmount({
       gnoUsdPrice,
       fourWeeksUsdVolumeThreshold,
       gnoBalance: 100,
       isOgNftHolder: false,
-      weekUsdVolume: 22_001,
-      fourWeeksUsdVolume: 23_000,
+      weekUsdVolume: 22_000.1,
+      fourWeeksUsdVolume: 22_000.1,
     });
     expect(rewardAmount).toBe(0);
   });
 
-  test('throws error if gnoUsdPrice <= 0', () => {
+  test('same 1-week and 4-weeks volumes below threshold ($22,000), 10 GNO balance with OG NFT: 3+1%', () => {
+    const weekUsdVolume = 1000;
+    const rewardAmount = calculateWeekRewardAmount({
+      gnoUsdPrice,
+      fourWeeksUsdVolumeThreshold,
+      gnoBalance: 10,
+      isOgNftHolder: true,
+      weekUsdVolume,
+      fourWeeksUsdVolume: weekUsdVolume,
+    });
+    const actualRewardPercentage = (rewardAmount * 100 * gnoUsdPrice) / weekUsdVolume;
+    expect(actualRewardPercentage).toBe(4);
+  });
+
+  test('1-week volume $1,000, 4-weeks volume $5,100 and 10 GNO balance: 3%', () => {
+    const weekUsdVolume = 1000;
+    const rewardAmount = calculateWeekRewardAmount({
+      gnoUsdPrice,
+      fourWeeksUsdVolumeThreshold,
+      gnoBalance: 10,
+      isOgNftHolder: false,
+      weekUsdVolume,
+      fourWeeksUsdVolume: 5100,
+    });
+    const actualRewardPercentage = (rewardAmount * 100 * gnoUsdPrice) / weekUsdVolume;
+    expect(actualRewardPercentage).toBe(3);
+  });
+
+  test('1-week volume ($32,000), 4-weeks volume ($32,000) and 10 GNO balance: 0%', () => {
+    const weekUsdVolume = 32_000;
+    const rewardAmount = calculateWeekRewardAmount({
+      gnoUsdPrice,
+      fourWeeksUsdVolumeThreshold,
+      gnoBalance: 10,
+      isOgNftHolder: false,
+      weekUsdVolume,
+      fourWeeksUsdVolume: weekUsdVolume,
+    });
+    expect(rewardAmount).toBe(0);
+  });
+
+  test('1-week volume ($32,000), 4-weeks volume ($31,000) and 10 GNO balance: 0%', () => {
+    const weekUsdVolume = 32_000;
+    const rewardAmount = calculateWeekRewardAmount({
+      gnoUsdPrice,
+      fourWeeksUsdVolumeThreshold,
+      gnoBalance: 10,
+      isOgNftHolder: false,
+      weekUsdVolume,
+      fourWeeksUsdVolume: weekUsdVolume - 1000,
+    });
+    expect(rewardAmount).toBe(0);
+  });
+
+  test('gnoUsdPrice <= 0: throws error', () => {
     expect(() =>
       calculateWeekRewardAmount({
         fourWeeksUsdVolumeThreshold,
@@ -82,7 +136,7 @@ describe('calculateWeekRewardAmount', () => {
     ).toThrow('gnoUsdPrice must be greater than 0');
   });
 
-  test('throws error if fourWeeksUsdVolumeThreshold <= 0', () => {
+  test('fourWeeksUsdVolumeThreshold <= 0: throws error', () => {
     expect(() =>
       calculateWeekRewardAmount({
         fourWeeksUsdVolumeThreshold: 0,
